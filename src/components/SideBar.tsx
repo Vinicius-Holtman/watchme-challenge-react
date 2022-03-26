09e0f7903c1fe3a42b4a79883a98d92a80a1e467
vinicius <viniholtman28@gmail.com>
2022-03-25 22:24:03 -0300

    Finalizado desafio 02 rocketseat

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { api } from "../services/api";
+import { GenreResponseProps } from "../types/GenreResponse";
+import { Button } from "./Button";
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  selectedGenreId: number;
+  setSelectedGenreId: Dispatch<SetStateAction<number>>;
+}
+
+export function SideBar({ selectedGenreId, setSelectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
